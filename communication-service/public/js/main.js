b26a07006ecfd802ae7ba7d32e662ea5b7829985
LAPTOP-3G8LIRN0\Lim Li Gang <ligangjurong8@gmail.com>
2022-09-23 13:47:19 +0800

    Basic chat app without MongoDB integration.

---
+++ communication-service/public/js/main.js
@@ -0,0 +1,69 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true // Ignore extraneous symbols like ampersands, question marks
+})
+
+const socket = io();
+
+// Join chatroom
+socket.emit('joinRoom', { username, room });
+
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+    outputRoomName(room);
+    outputUsers(users);
+});
+
+// Message from server
+socket.on('message', message => {
+    console.log(message);
+    outputMessage(message);
+
+    // Scroll down upon receiving a message
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+// Message submit
+chatForm.addEventListener('submit', (e) => {
+    // Prevent submission to form from automatically submitting to a file
+    e.preventDefault();
+
+    // Get message from text input
+    const msg = e.target.elements.msg.value;
+
+    // Emit message to server
+    socket.emit('chatMessage', msg);
+
+    // Clear input
+    e.target.elements.msg.value = '';
+    // Focus on new empty input
+    e.target.elements.msg.focus();
+});
+
+// Output message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+						<p class="text">
+							${message.text}
+						</p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+    roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+    userList.innerHTML = `
+        ${users.map(user => `<li>${user.username}</li>`).join('')}
+    `;
+}
