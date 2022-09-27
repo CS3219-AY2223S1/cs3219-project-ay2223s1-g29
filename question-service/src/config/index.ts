b213c09b1942ab552acaa544a0b582231ecfcd65
yanchenglee@gmail.com <yanchenglee@gmail.com>
2022-09-27 19:26:37 +0800

    feat(question-service): add gcloud app deploy files

---
+++ question-service/src/config/index.ts
@@ -0,0 +1,14 @@
+import * as dotenv from 'dotenv';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const envFound = dotenv.config();
+if (!envFound) {
+  // Throw generic error
+  throw new Error("Couldn't find .env file");
+}
+
+export default {
+  // Application port.
+  port: parseInt(process.env.PORT) || 7001,
+};
