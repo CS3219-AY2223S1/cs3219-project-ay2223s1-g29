1ce8d0f63b3767a13966e363f1706127aaf1bd91
Jiefeng <lowjiefeng1998@gmail.com>
2022-10-17 13:41:10 +0800

    feat(fe); add reset of user service

--- frontend/src/components/user/UserHomeCard.tsx
+++ frontend/src/components/user/UserHomeCard.tsx
@@ -1,31 +1,64 @@
-import { Box, Button, Flex, Heading } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  Heading,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Text,
+} from '@chakra-ui/react';
 import React, { useCallback } from 'react';
-import { FaUser } from 'react-icons/fa';
+import { FaHandsHelping, FaUser } from 'react-icons/fa';
 import { useAuth } from '../../context/AuthContext';
 
 type UserHomeCardProps = {
   username: string;
+  onManageAccount?: () => void;
+  onLogout?: () => void;
 };
 
 export default function UserHomeCard(props: UserHomeCardProps) {
   const { username } = props;
-  const { clearAuth } = useAuth();
-
-  const onLogout = useCallback(() => {
-    clearAuth();
-    window.location.reload();
-  }, []);
 
   return (
     <Flex direction="column" rowGap={2}>
       <Heading display="flex" columnGap={2}>
-        <FaUser />
-        {username}
+        Peer Prep
+        <FaHandsHelping />
       </Heading>
 
-      <Button colorScheme="teal" variant="outline" size="sm" type="button" onClick={onLogout}>
-        Logout
-      </Button>
+      <Text fontSize="md" display="flex" columnGap={1} alignItems="center" justifyContent="center">
+        Hello, <FaUser />
+        {username}.
+      </Text>
+
+      {props.onManageAccount && (
+        <Button
+          colorScheme="teal"
+          variant="outline"
+          size="sm"
+          type="button"
+          onClick={props.onManageAccount}
+        >
+          Manage Account
+        </Button>
+      )}
+
+      {props.onLogout && (
+        <Button
+          colorScheme="teal"
+          variant="outline"
+          size="sm"
+          type="button"
+          onClick={props.onLogout}
+        >
+          Logout
+        </Button>
+      )}
     </Flex>
   );
 }
