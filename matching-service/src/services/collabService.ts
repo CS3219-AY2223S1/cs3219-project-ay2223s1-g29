33ebba7f733393d5d7b8e3ae90f157af2ab705f9
yanchenglee@gmail.com <yanchenglee@gmail.com>
2022-11-03 15:43:32 +0800

    chore(change url link for collab service)

--- matching-service/src/services/collabService.ts
+++ matching-service/src/services/collabService.ts
@@ -1,7 +1,7 @@
 import Queue from 'queue-fifo';
 import axios from 'axios';
 
-const baseUrl = "https://cs3219-collab-service-dot-cs3219-361208.as.r.appspot.com/api/matches"; // change to collab service url
+const baseUrl = "https://collab.schoolex.me/api/matches"; // change to collab service url
 
 const createMatch = (userId1:string, userId2:string, difficulty:string) => {
     return axios.post(baseUrl, {userId1: userId1, userId2:userId2, difficulty:difficulty});
