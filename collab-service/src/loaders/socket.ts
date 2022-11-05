61aa8e0629074c74cd5af0c6871eb6f1518b1bdc
Biao Yi <schoolex@hotmail.com>
2022-11-06 00:19:41 +0800

    refactor(collab): clean up unused code

--- collab-service/src/loaders/socket.ts
+++ collab-service/src/loaders/socket.ts
@@ -1,7 +1,4 @@
-import mongoose from 'mongoose';
-import { Document, DocumentModel } from '../models/document';
-import config from '../config';
-import * as express from 'express';
+import { logger } from '../loggers/logger';
 
 export default async (server) => {
   const io = require('socket.io')(server, {
@@ -12,37 +9,14 @@ export default async (server) => {
     transports: ['websocket'],
   });
 
-  const defaultValue = '';
-
   io.on('connection', (socket) => {
     socket.on('join-room', async (roomId) => {
-      	console.log('joined room');
-	socket.join(roomId);
-	const clients = io.sockets.adapter.rooms.get(roomId);
-	console.log(clients);
-	socket.in(roomId).emit('user-join',[...clients]);
-    });
-
-    socket.on('get-document', async (documentId) => {
-      const document = await findOrCreateDocument(documentId);
-      socket.emit('load-document', 'test');
-
-      socket.on('send-changes', (delta) => {
-        socket.broadcast.to(documentId).emit('receive-changes', delta);
-      });
-
-      socket.on('save-document', async (data, docId) => {
-        // console.log(docId, data)
-        await DocumentModel.findOneAndUpdate({ id: docId }, { data });
-      });
+      logger.info(`Socket ${socket.id} joined room ${roomId}`);
+      socket.join(roomId);
+      const clients = io.sockets.adapter.rooms.get(roomId);
+      logger.info(`Room ${roomId} has ${clients.size} clients`);
+      socket.in(roomId).emit('user-join', [...clients]);
     });
   });
 
-  async function findOrCreateDocument(id) {
-    if (id == null) return;
-
-    const document = await DocumentModel.findOne({ id });
-    if (document) return document;
-    return await DocumentModel.create({ id, data: defaultValue });
-  }
 };
